0316a4f7c8a6ee5727196d6b571863f47c02b095
Daniel Shin <danielshin234@gmail.com>
2018-11-15 13:08:16 -0800

    set up templates

--- surveillance.cpp
+++ surveillance.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "surveillance.hpp"
+
+vector<string> surveillance::visited_kb_tavern() {
+    return vector<string>();
+}
+
+vector<string> surveillance::not_visited_kb_home() {
+    return vector<string>();
+}
+
+vector<string> surveillance::visited_kb_school_not_tavern_home() {
+    return vector<string>();
+}
+
+vector<string> surveillance::visited_all_remove() {
+    return vector<string>();
+}
--- surveillance.hpp
+++ surveillance.hpp
@@ -5,9 +5,19 @@
 #ifndef SURVEILLANCE_SURVEILLANCE_HPP
 #define SURVEILLANCE_SURVEILLANCE_HPP
 
+#include <string>
+#include <vector>
+
+using namespace std;
 
 class surveillance {
+    vector<string> visited_kb_tavern();
 
+    vector<string> not_visited_kb_home();
+
+    vector<string> visited_kb_school_not_tavern_home();
+
+    vector<string> visited_all_remove();
 };
 
 

b43f9a86572576e2dab6c52f520ab1e0999cbb2b
Daniel Shin <danielshin234@gmail.com>
2018-11-15 12:40:56 -0800

    init

---
+++ cmake-build-debug/CMakeFiles/Surveillance.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Surveillance.dir/main.cpp.o"
+  "Surveillance.pdb"
+  "Surveillance.exe"
+  "libSurveillance.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Surveillance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Surveillance.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Surveillance.
+# This may be replaced when dependencies are built.
